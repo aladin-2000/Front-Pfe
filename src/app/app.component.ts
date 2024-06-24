f5884c5592ed26e187e3254b2b6635ce1e243fcd
aladin-2000 <alaaeddine.saidani@etudinat-enit.utm.tn>
2024-06-24 10:29:07 +0100

    secondCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,TopbarComponent,NavbarComponent,FooterComponent,MenuComponent,WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
