35cf4a62724a852406b38d0be726d7d135a86365
Safa Hamad <safa.hamad@se.linedata.com>
2024-07-15 16:00:03 +0100

    footer ready to use, header still needing some modifications

--- src/app/header/topbar/topbar.component.ts
+++ src/app/header/topbar/topbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-topbar',
   standalone: true,
@@ -8,5 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './topbar.component.css'
 })
 export class TopbarComponent {
-
+  onSearch() {
+    // Add your search logic here
+    console.log('Performing search');
+  }
 }
