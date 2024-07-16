9b95750d39fa91315912284ad1abd0e2f2d2158b
Safa Hamad <safa.hamad@se.linedata.com>
2024-07-16 16:52:34 +0100

    Changes in the topbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { FooterComponent } from './footer/footer.component';
 import { TopbarComponent } from './header/topbar/topbar.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
+import { ProfileDropdownComponent } from './header/topbar/profile-dropdown/profile-dropdown.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TopbarComponent,NavbarComponent,FooterComponent,WelcomeComponent,HeaderComponent],
+  imports: [RouterOutlet,TopbarComponent,NavbarComponent,FooterComponent,WelcomeComponent,HeaderComponent, ProfileDropdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/header/topbar/topbar.component.ts
+++ src/app/header/topbar/topbar.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-
+import { ProfileDropdownComponent } from './profile-dropdown/profile-dropdown.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [ProfileDropdownComponent, CommonModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
 export class TopbarComponent {
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    console.log('Toggle dropdown method called');
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown() {
+    console.log('Close dropdown method called');
+    this.isDropdownOpen = false;
+  }
+
+  onLogout() {
+    // Add your logout logic here
+    console.log('Logout clicked');
+  }
+
   onSearch() {
     // Add your search logic here
     console.log('Performing search');
