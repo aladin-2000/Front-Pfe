a64e4da30076a9b42581cca17d897cdd92f12659
Safa Hamad <safa.hamad@se.linedata.com>
2024-07-18 14:38:33 +0100

    jeudi 18-07-2024

--- src/app/shared/components/header/navbar/navbar.component.ts
+++ src/app/shared/components/header/navbar/navbar.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule,
+    MatButtonModule,
+
+    
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
--- src/app/shared/components/header/topbar/topbar.component.ts
+++ src/app/shared/components/header/topbar/topbar.component.ts
@@ -1,16 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProfileDropdownComponent } from './profile-dropdown/profile-dropdown.component';
 import { CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [ProfileDropdownComponent, CommonModule],
+  imports: [ProfileDropdownComponent,
+    CommonModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
-export class TopbarComponent {
+export class TopbarComponent implements OnInit{
   isDropdownOpen = false;
+  options: string[] = ['ALOT', 'AFLH']; 
+  filteredOptions!: Observable<string[]>;
+
+  searchControl = new FormControl();
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.filteredOptions = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300), // Optional: Debounce time to wait for user input to settle
+      distinctUntilChanged(), // Optional: Only emit when the filter value changes
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().startsWith(filterValue));
+  }
+  
 
   toggleDropdown() {
     console.log('Toggle dropdown method called');
@@ -31,4 +63,5 @@ export class TopbarComponent {
     // Add your search logic here
     console.log('Performing search');
   }
+
 }
