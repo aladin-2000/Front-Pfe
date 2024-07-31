2a90e898fb1194ce22db9548854eb0bd29d433aa
mohamed.kessenti <mohamed.kessenti@se.linedata.com>
2024-07-31 10:19:43 +0100

    fixed the first values list in the form

--- src/app/features/alot-page/views/alot-form/viewmodel/alot-form.viewmodel.ts
+++ src/app/features/alot-page/views/alot-form/viewmodel/alot-form.viewmodel.ts
@@ -1,1 +1,20 @@
-export class AlotFormViewModel{}
+import { map, Observable } from "rxjs";
+import { AlotService } from "../../../../../core/services/alot.service";
+import { Injectable } from "@angular/core";
+@Injectable({
+    providedIn: 'root'
+  })
+export class AlotFormViewModel {
+    constructor(private alotService: AlotService) { }
+    listOfCreatedWallets(): Observable<{ data: any[]; columns: string[] }> { 
+        return this.alotService.listOfCreatedWallets().pipe(
+            map((data: any[]) => {
+                let columns: string[] = [];
+                if (data.length > 0) {
+                  columns = Object.keys(data[0]);
+                }
+                const updatedData=data.map(item=>({...item,isSelected:false}))
+                return { data:updatedData, columns };
+              })
+            );
+}}
